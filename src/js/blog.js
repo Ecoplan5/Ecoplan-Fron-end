5a822d1e725a858740b1df2e1c48dc6dcfd6c7e5
Ecoplan5 <ecoplan38@gmail.com>
2024-11-26 11:22:42 -0500

    actualicé landing, creé index y blog

---
+++ src/js/blog.js
@@ -0,0 +1,16 @@
+document.getElementById('searchForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const query = document.getElementById('searchInput').value.toLowerCase();
+    const articles = document.querySelectorAll('#articlesContainer .col');
+
+    articles.forEach(article => {
+        const title = article.querySelector('.card-title').textContent.toLowerCase();
+        const description = article.querySelector('.card-text').textContent.toLowerCase();
+
+        if (title.includes(query) || description.includes(query)) {
+            article.style.display = 'block';
+        } else {
+            article.style.display = 'none';
+        }
+    });
+});
